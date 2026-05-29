029a8bd72a30d873188c0d0aee36be7ebb5d733b
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-05-28 21:39:01 -0500

    refactor: move nixpkgs channel input to dedicated channels.nix repository

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   # default pkgs channel
-  inputs.channel.url = "github:nixology/core.nix?dir=partitions/channels/nixpkgs-unstable";
+  inputs.channel.url = "github:nixology/channels.nix?dir=nixpkgs-unstable";
 
   inputs.nixpkgs.follows = "channel/nixpkgs";
 
