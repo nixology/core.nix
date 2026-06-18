1ce30aff2b7aae528a20fdf91649522bb357acdc
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-06-18 11:55:23 -0500

    refactor: rename nixpkgs-unstable input to default

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
-  description = "A collection of flake components for various purposes.";
+  description = "A collection of core components for nixology.";
 
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   # default channel
-  inputs.nixpkgs-unstable.url = "github:nixology/channels.nix?dir=nixpkgs-unstable";
+  inputs.default.url = "github:nixology/channels.nix?dir=nixpkgs-unstable";
 
   # follow the default channel for nixpkgs
-  inputs.nixpkgs.follows = "nixpkgs-unstable/channel";
+  inputs.nixpkgs.follows = "default/channel";
 
   outputs =
     inputs:
